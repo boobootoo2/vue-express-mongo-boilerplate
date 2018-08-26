d9aa1fca5e7914af4cc92055c53237461b9bd366
Jack Shultz <jackygrahamez@gmail.com>
2018-08-25 21:07:21 -0400

    updated gallery

--- client/app/core/components/sidebar/index.vue
+++ client/app/core/components/sidebar/index.vue
@@ -9,23 +9,11 @@
 							i.fa.fa-home
 						span.label {{ "Home" | i18n }}
 
-				router-link(tag="li", to="/counter")
-					a(:title="_('Demo')")
+				router-link(tag="li", to="/gallery")
+					a(:title="_('Gallery Test')")
 						span.icon
-							i.fa.fa-tasks
-						span.label {{ "Demo" | i18n }}
-
-				router-link(tag="li", to="/devices")
-					a(:title="_('Devices')")
-						span.icon
-							i.fa.fa-tablet
-						span.label {{ "Devices" | i18n }}
-
-				router-link(tag="li", to="/posts")
-					a(:title="_('Posts')")
-						span.icon
-							i.fa.fa-comments
-						span.label {{ "Posts" | i18n }}
+							i.fa.fa-home
+						span.label {{ "Gallery Test" | i18n }}
 
 			.title {{ "Profile" | i18n }}
 			ul
@@ -47,14 +35,13 @@
 </template>
 
 <script>
-
-	export default {
-		props: [
-			"minimized"
-		]
-	};
-	
+    export default {
+        props: [
+            "minimized"
+        ]
+    };
 </script>
 
 <style lang="scss">
+
 </style>
