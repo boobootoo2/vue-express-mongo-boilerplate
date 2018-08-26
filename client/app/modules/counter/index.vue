9263b91d67632a20e855f43a0efc232a55d25aa6
Jack Shultz <jackygrahamez@gmail.com>
2018-08-25 20:55:17 -0400

    added a gallery

--- client/app/modules/counter/index.vue
+++ client/app/modules/counter/index.vue
@@ -3,91 +3,120 @@
 		h2.title {{ "Demo" | i18n }}
 
 		h3 {{ count }}
-		button.button.success(@click="inc") 
+		button.button.success(@click="inc")
 			span.icon
-				i.fa.fa-arrow-up 
+				i.fa.fa-arrow-up
 			span {{ "Increment" | i18n }}
 		br
 		br
-		button.button.warning(@click="dec") 
+		button.button.warning(@click="dec")
 			span
-				i.fa.fa-arrow-up  
+				i.fa.fa-arrow-up
 			span {{ "Decrement" | i18n }}
+		gallery(:images="images" :index="index" @close="index = null")
+		div(class="image"
+			v-for="(image, imageIndex) in images"
+			:key="imageIndex"
+			@click="index = imageIndex"
+			:style="{ backgroundImage: 'url(' + image + ')', width: '300px', height: '200px' }")
+
+
 
 </template>
 
 <script>
-	import { mapActions, mapGetters } from "vuex";
+    import {
+        mapActions,
+        mapGetters
+    } from "vuex";
 
-	import Service from "../../core/service";
+    import VueGallery from 'vue-gallery';
 
-	export default {
-		/**
-		 * Computed getters
-		 */
-		 computed: mapGetters("counter", [
-			 "count"
-		 ]),
+    import Service from "../../core/service";
 
-		/**
-		 * Page methods
-		 */
-		methods: {
-			/**
-			 * Actions from store
-			 */
-			...mapActions("counter", [
-				"getValue",
-				"increment",
-				"decrement",
-				"changedValue"
-			]),
+    export default {
+        data: function() {
+            return {
+                images: [
+                    'https://dummyimage.com/800/ffffff/000000',
+                    'https://dummyimage.com/1600/ffffff/000000',
+                    'https://dummyimage.com/1280/000000/ffffff',
+                    'https://dummyimage.com/400/000000/ffffff',
+                ],
+                index: null
+            };
+        },
 
-			/**
-			 * Increment counter
-			 */
-			inc() {
-				this.increment();
-			},
+        components: {
+            'gallery': VueGallery
+        },
 
-			/**
-			 * Decrement counter
-			 */
-			dec() {
-				this.decrement();
-			}
-		},
+        /**
+         * Computed getters
+         */
+        computed: mapGetters("counter", [
+            "count"
+        ]),
 
-		/**
-		 * Socket handlers. Every property is an event handler
-		 */
-		socket: {
+        /**
+         * Page methods
+         */
+        methods: {
+            /**
+             * Actions from store
+             */
+            ...mapActions("counter", [
+                "getValue",
+                "increment",
+                "decrement",
+                "changedValue"
+            ]),
 
-			prefix: "/counter/",
+            /**
+             * Increment counter
+             */
+            inc() {
+                this.increment();
+            },
 
-			//namespace: "/counter",
+            /**
+             * Decrement counter
+             */
+            dec() {
+                this.decrement();
+            }
+        },
 
-			events: {
-				/**
-				 * Counter value is changed
-				 * @param  {Number} msg Value of counter
-				 */
-				changed(res) {
-					console.log("Counter changed to " + res.data + (res.user ? " by " + res.user.fullName : ""));
-					this.changedValue(res.data);
-				}
-			}
-		},
+        /**
+         * Socket handlers. Every property is an event handler
+         */
+        socket: {
 
-		created() {
-			this.$service = new Service("counter", this); 
-			
-			// Get the latest value of counter
-			this.getValue(); 
-		}
-	};
+            prefix: "/counter/",
 
+            //namespace: "/counter",
+
+            events: {
+                /**
+                 * Counter value is changed
+                 * @param  {Number} msg Value of counter
+                 */
+                changed(res) {
+                    console.log("Counter changed to " + res.data + (res.user ? " by " + res.user.fullName : ""));
+                    this.changedValue(res.data);
+                }
+            }
+        },
+
+        created() {
+            this.$service = new Service("counter", this);
+
+            // Get the latest value of counter
+            this.getValue();
+        }
+    };
 </script>
 
 <style lang="scss" scoped>
+
 </style>
---
+++ client/app/modules/gallery/index.vue
@@ -0,0 +1,44 @@
+<template lang="pug">
+	.container
+		gallery(:images="images" :index="index" @close="index = null")
+		div(class="image"
+		v-for="(image, imageIndex) in images"
+		:key="imageIndex"
+		@click="index = imageIndex"
+		:style="{ backgroundImage: 'url(' + image + ')', width: '300px', height: '200px' }")
+
+
+
+</template>
+
+<script>
+    import VueGallery from 'vue-gallery';
+
+    import Service from "../../core/service";
+
+    export default {
+        data: function() {
+            return {
+                images: [
+                    "http://www.shultzexpeditions.org/Brodheads%202014/slides/DSCF0789.JPG", "http://www.shultzexpeditions.org/Brodheads%202014/slides/DSCF0791.JPG", "http://www.shultzexpeditions.org/Brodheads%202014/slides/DSCF0792.JPG", "http://www.shultzexpeditions.org/Brodheads%202014/slides/DSCF0793.JPG", "http://www.shultzexpeditions.org/Brodheads%202014/slides/DSCF0794.JPG", "http://www.shultzexpeditions.org/Brodheads%202014/slides/DSCF0795.JPG", "http://www.shultzexpeditions.org/Brodheads%202014/slides/DSCF0796.JPG", "http://www.shultzexpeditions.org/Brodheads%202014/slides/DSCF0798.JPG", "http://www.shultzexpeditions.org/Brodheads%202014/slides/DSCF0799.JPG", "http://www.shultzexpeditions.org/Brodheads%202014/slides/DSCF0800.JPG", "http://www.shultzexpeditions.org/Brodheads%202014/slides/DSCF0801.JPG", "http://www.shultzexpeditions.org/Brodheads%202014/slides/DSCF0804.JPG", "http://www.shultzexpeditions.org/Brodheads%202014/slides/DSCF0806.JPG", "http://www.shultzexpeditions.org/Brodheads%202014/slides/DSCF0808.JPG", "http://www.shultzexpeditions.org/Brodheads%202014/slides/DSCF0810.JPG", "http://www.shultzexpeditions.org/Brodheads%202014/slides/DSCF0811.JPG", "http://www.shultzexpeditions.org/Brodheads%202014/slides/DSCF0812.JPG", "http://www.shultzexpeditions.org/Brodheads%202014/slides/DSCF0816.JPG", "http://www.shultzexpeditions.org/Brodheads%202014/slides/DSCF0821.JPG", "http://www.shultzexpeditions.org/Brodheads%202014/slides/DSCF0824.JPG", "http://www.shultzexpeditions.org/Brodheads%202014/slides/DSCF0825.JPG", "http://www.shultzexpeditions.org/Brodheads%202014/slides/DSCF0827.JPG", "http://www.shultzexpeditions.org/Brodheads%202014/slides/DSCF0829.JPG", "http://www.shultzexpeditions.org/Brodheads%202014/slides/DSCF0831.JPG", "http://www.shultzexpeditions.org/Brodheads%202014/slides/DSCF0832.JPG"
+                ],
+                index: null
+            };
+        },
+
+        components: {
+            'gallery': VueGallery
+        },
+    };
+</script>
+
+<style lang="scss" scoped>
+    .image {
+        float: left;
+        background-size: cover;
+        background-repeat: no-repeat;
+        background-position: center center;
+        border: 1px solid #ebebeb;
+        margin: 5px;
+    }
+</style>
